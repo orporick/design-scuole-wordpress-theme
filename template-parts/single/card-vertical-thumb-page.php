090675ca1adb4ea9653b7c2eaf2ff386a942d57a
Riccardo Giannitrapani <riccardo.giannitrapani@gmail.com>
2023-06-23 00:36:57 +0200

    Risolto bug che non mostrava l'abstract nelle notizie

--- template-parts/single/card-vertical-thumb-page.php
+++ template-parts/single/card-vertical-thumb-page.php
@@ -5,11 +5,16 @@ $autore = get_user_by("ID", $post->post_author);
 $image_id= get_post_thumbnail_id($post);
 $image_url = get_the_post_thumbnail_url($post, "vertical-card");
 
-?><div class="card card-bg card-vertical-thumb bg-white card-thumb-rounded">
+$excerpt =  dsi_get_meta("descrizione", "", $post->ID);
+if(!$excerpt)
+    $excerpt = get_the_excerpt($post);
+?>
+
+<div class="card card-bg card-vertical-thumb bg-white card-thumb-rounded">
 	<div class="card-body">
 		<div class="card-content">
 			<h3 class="h5"><a href="<?php echo get_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h3>
-			<p><?php echo get_the_excerpt($post); ?></p>
+			<p><?php echo $excerpt; ?></p>
 		</div>
 		<?php if($image_url) { ?>
 			<div class="card-thumb">
